66c6bdc51206a9bd5e1d7953393a4397cf0bd385
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-12-19 16:12:44 +0100

    feat: move requests
    Move `Request`s to their own category.
    Include the correct new file for the usages.
    
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- plugins/builtin/source/content/providers/file_provider.cpp
+++ plugins/builtin/source/content/providers/file_provider.cpp
@@ -6,6 +6,7 @@
 #include <hex/api/localization_manager.hpp>
 #include <hex/api/project_file_manager.hpp>
 #include <hex/api/task_manager.hpp>
+#include <hex/api/events/requests.hpp>
 
 #include <popups/popup_question.hpp>
 #include <toasts/toast_notification.hpp>
